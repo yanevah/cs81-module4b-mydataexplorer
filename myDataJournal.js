3bcc53934312832dc801fbeaace770a656269fb8
Nina <janev_nina_alexandra@student.smc.edu>
2026-01-18 10:20:29 -0800

    Add predictions about weekly data

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,6 +1,7 @@
 // GitHub Repo URL: https://github.com/yanevah/cs81-module4b-mydataexplorer
 
 // Weekly personal data journal
+// Each object represents one day and tracks sleep, screen time, mood, caffeine, and focus.
 const weekData = [
   {
     day: "Monday",
@@ -28,7 +29,7 @@ const weekData = [
   },
   {
     day: "Thursday",
-    sleepHours: 7.5,
+    sleepHours: 6.5,
     screenTime: 5,
     mood: "focused",
     caffeineIntake: 1,
@@ -59,3 +60,75 @@ const weekData = [
     focusLevel: 6
   }
 ];
+
+// PREDICTION:
+// I predict Wednesday will have the highest screen time because it is my busiest workday.
+function findHighestScreenTime(data) {
+  let maxDay = data[0];
+
+  for (let day of data) {
+    if (day.screenTime > maxDay.screenTime) {
+      maxDay = day;
+    }
+  }
+
+  return maxDay;
+}
+
+// PREDICTION:
+// I expect my average sleep to be just under 7 hours since I sleep more on weekends.
+function averageSleep(data) {
+  let total = 0;
+
+  for (let day of data) {
+    total += day.sleepHours;
+  }
+
+  return (total / data.length).toFixed(1);
+}
+
+// PREDICTION:
+// I think my most frequent mood will be productive due to weekday routines.
+function mostFrequentMood(data) {
+  const moodCounts = {};
+
+  for (let day of data) {
+    moodCounts[day.mood] = (moodCounts[day.mood] || 0) + 1;
+  }
+
+  let topMood = null;
+  let maxCount = 0;
+
+  for (let mood in moodCounts) {
+    if (moodCounts[mood] > maxCount) {
+      topMood = mood;
+      maxCount = moodCounts[mood];
+    }
+  }
+
+  return topMood;
+}
+
+// PREDICTION:
+// I believe higher caffeine intake does NOT improve focus and may actually lower it.
+function correlateCaffeineToFocus(data) {
+  let highCaffeineFocus = 0;
+  let lowCaffeineFocus = 0;
+  let highDays = 0;
+  let lowDays = 0;
+
+  for (let day of data) {
+    if (day.caffeineIntake >= 2) {
+      highCaffeineFocus += day.focusLevel;
+      highDays++;
+    } else {
+      lowCaffeineFocus += day.focusLevel;
+      lowDays++;
+    }
+  }
+
+  return highCaffeineFocus / highDays > lowCaffeineFocus / lowDays;
+}
+
+
+
