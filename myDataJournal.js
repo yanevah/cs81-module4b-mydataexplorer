ada39c4a94e283f0f0dcdff7381b013ad6290328
Nina <janev_nina_alexandra@student.smc.edu>
2026-01-18 10:11:40 -0800

    Add weekly data journal structure

---
+++ myDataJournal.js
@@ -0,0 +1,61 @@
+// GitHub Repo URL: https://github.com/yanevah/cs81-module4b-mydataexplorer
+
+// Weekly personal data journal
+const weekData = [
+  {
+    day: "Monday",
+    sleepHours: 6.5,
+    screenTime: 7,
+    mood: "tired",
+    caffeineIntake: 2,
+    focusLevel: 5
+  },
+  {
+    day: "Tuesday",
+    sleepHours: 7,
+    screenTime: 6,
+    mood: "productive",
+    caffeineIntake: 1,
+    focusLevel: 7
+  },
+  {
+    day: "Wednesday",
+    sleepHours: 6,
+    screenTime: 8,
+    mood: "stressed",
+    caffeineIntake: 2,
+    focusLevel: 4
+  },
+  {
+    day: "Thursday",
+    sleepHours: 7.5,
+    screenTime: 5,
+    mood: "focused",
+    caffeineIntake: 1,
+    focusLevel: 8
+  },
+  {
+    day: "Friday",
+    sleepHours: 6,
+    screenTime: 7,
+    mood: "distracted",
+    caffeineIntake: 3,
+    focusLevel: 5
+  },
+  {
+    day: "Saturday",
+    sleepHours: 8,
+    screenTime: 4,
+    mood: "relaxed",
+    caffeineIntake: 2,
+    focusLevel: 7
+  },
+  {
+    day: "Sunday",
+    sleepHours: 7.5,
+    screenTime: 4,
+    mood: "calm",
+    caffeineIntake: 2,
+    focusLevel: 6
+  }
+];
